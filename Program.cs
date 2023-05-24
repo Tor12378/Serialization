bf9eb7812f7c1c46324627f4265c4725677aa18b
Тимур <tupaev05@mail.ru>
2023-05-24 16:37:19 +0300

    /

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[]
+            {
+            new Person { Name = "max1", Age = 30, Addresses = new List<string> { "Address 1", "Address 2" } },
+            new Person { Name = "ivan", Age = 25, Addresses = new List<string> { "Address 3", "Address 4" } }
+            };
+
+            string filePath = "persons.json";
+
+            
+            Serialization.SerializeToJson(persons, filePath);
+
+           
+            Person[] deserializedPersons = Serialization.DeserializeFromJson<Person[]>(filePath);
+
+            
+
+            foreach (var person in deserializedPersons)
+            {
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+
+                Console.WriteLine("Addresses:");
+                foreach (var address in person.Addresses)
+                {
+                    Console.WriteLine(address);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Serialization.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    public class Serialization
+    {
+        public static void SerializeToJson<T>(T obj, string filePath)
+        {
+            string json = JsonConvert.SerializeObject(obj);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static T DeserializeFromJson<T>(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            T obj = JsonConvert.DeserializeObject<T>(json);
+            return obj;
+        }
+    }
+}
